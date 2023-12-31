d4ec7da255e4362f2d032be8220cb9a194b86e47
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 17:49:23 +0600

    Zod schema defined for sign in and connect this controller to the route

--- src/app/modules/auth/route.ts
+++ src/app/modules/auth/route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { ZSingIn, ZSingUp } from "./validation";
 import validateRequest from "../../middlewares/validateRequest";
-import { ZSingUp } from "./validation";
 
 const router = Router();
 
-const { signUp } = AuthController;
+const { signUp, signIn } = AuthController;
 
-router.post("/signup", validateRequest(ZSingUp), signUp);
+router
+  .post("/signup", validateRequest(ZSingUp), signUp)
+  .post("/signin", validateRequest(ZSingIn), signIn);
 
 export const AuthRoutes = router;
--- src/app/modules/auth/validation.ts
+++ src/app/modules/auth/validation.ts
@@ -8,4 +8,11 @@ const ZSingUp = z.object({
   }),
 });
 
-export { ZSingUp };
+const ZSingIn = z.object({
+  body: z.object({
+    email: z.string({ required_error: "Email is required!!" }).email(),
+    password: z.string({ required_error: "Password is required!!" }),
+  }),
+});
+
+export { ZSingUp, ZSingIn };
