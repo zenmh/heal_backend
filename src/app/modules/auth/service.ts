3b4b5649888881e549dca4c5a00475addd54ce63
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 17:41:39 +0600

    Sign in function developed in service

--- src/app/modules/auth/service.ts
+++ src/app/modules/auth/service.ts
@@ -1,9 +1,9 @@
 import { User } from "@prisma/client";
-import { ISignUpResponse } from "./interface";
 import prisma from "../../../constants/prisma";
 import ApiError from "../../../errors/ApiError";
 import { createToken } from "../../../helpers/jwt";
-import { hashPassword } from "../../../helpers/bcrypt";
+import { hashPassword, matchPassword } from "../../../helpers/bcrypt";
+import { ISignIn, ISignInResponse, ISignUpResponse } from "./interface";
 
 const signUp = async (data: User): Promise<ISignUpResponse> => {
   const isExist = await prisma.user.findFirst({
@@ -26,6 +26,30 @@ const signUp = async (data: User): Promise<ISignUpResponse> => {
   );
 
   return { accessToken, refreshToken, result: user };
+};
+
+const singIn = async ({
+  email,
+  password,
+}: ISignIn): Promise<ISignInResponse> => {
+  const user = await prisma.user.findFirst({ where: { email } });
+
+  if (!user) throw new ApiError(404, "User not found!!");
+
+  const isPasswordMatch = await matchPassword(password, user.password);
+
+  if (!isPasswordMatch) throw new ApiError(400, "Password is incorrect!!");
+
+  const accessToken = createToken(
+    { userId: user.id, role: user.role },
+    "access"
+  );
+  const refreshToken = createToken(
+    { userId: user.id, role: user.role },
+    "refresh"
+  );
+
+  return { accessToken, refreshToken };
 };
 
 export const AuthService = { signUp };
