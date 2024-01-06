7c4cc0374f22ea51161f61ceb3f2d9e951e2fada
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-06 19:40:29 +0600

    Secure the doctor creation route and allowed only for ADMIN and SUPER_ADMIN

--- src/app/modules/doctor/route.ts
+++ src/app/modules/doctor/route.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { ZCreateDoctor } from "./validation";
 import { DoctorController } from "./controller";
+import auth from "../../middlewares/auth";
+import { Role } from "@prisma/client";
 
 const router = Router();
 const { createDoctor } = DoctorController;
+const { ADMIN, SUPER_ADMIN } = Role;
 
-router.post("/", validateRequest(ZCreateDoctor), createDoctor);
+router.post(
+  "/",
+  auth(ADMIN, SUPER_ADMIN),
+  validateRequest(ZCreateDoctor),
+  createDoctor
+);
 
 export const DoctorRoutes = router;
