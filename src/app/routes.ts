618d28569c5122295bab2d0a81e7711fb3321021
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-06 19:33:32 +0600

    Connect the doctor routes to global route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthRoutes } from "./modules/auth/route";
+import { DoctorRoutes } from "./modules/doctor/route";
 
 const router = Router();
 
-[{ path: "/auth", route: AuthRoutes }].forEach(({ path, route }) =>
-  router.use(path, route)
-);
+[
+  { path: "/auth", route: AuthRoutes },
+  { path: "/doctor", route: DoctorRoutes },
+].forEach(({ path, route }) => router.use(path, route));
 
 export const routes = router;
