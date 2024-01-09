d959d17b425005efaaf8bc0bcdabad960417cc42
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 20:02:30 +0600

    Handled  the error of failed to book appointment

--- src/app/modules/appointment/service.ts
+++ src/app/modules/appointment/service.ts
@@ -1,10 +1,13 @@
 import { Appointment } from "@prisma/client";
 import prisma from "../../../constants/prisma";
+import ApiError from "../../../errors/ApiError";
 
 const createAppointment = async (data: Appointment): Promise<Appointment> => {
-  const result = await prisma.appointment.create({ data });
+  const appointment = await prisma.appointment.create({ data });
 
-  return result;
+  if (!appointment) throw new ApiError(400, "Failed to book appointment!!");
+
+  return appointment;
 };
 
 export const AppointmentService = { createAppointment };
