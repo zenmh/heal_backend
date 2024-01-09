ecd66609f6668376a5340b92a2496197007c47b7
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-10 00:54:26 +0600

    Added the zod validation for book an appointment

--- src/app/modules/appointment/route.ts
+++ src/app/modules/appointment/route.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 import { AppointmentController } from "./controller";
 import { Role } from "@prisma/client";
 import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { ZBookAnAppointment } from "./validation";
 
 const router = Router();
 const { PATIENT, DOCTOR, ADMIN, SUPER_ADMIN } = Role;
 const { bookAnAppointment } = AppointmentController;
 
-router.post("/", auth(PATIENT, DOCTOR, ADMIN, SUPER_ADMIN), bookAnAppointment);
+router.post(
+  "/",
+  auth(PATIENT, DOCTOR, ADMIN, SUPER_ADMIN),
+  validateRequest(ZBookAnAppointment),
+  bookAnAppointment
+);
 
 export const AppointmentRoutes = router;
